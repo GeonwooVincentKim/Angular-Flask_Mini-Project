869f1dee08b23e5749b1f77053c094efdeef8e3f
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-25 15:06:13 +0900

    About the role of 'os.path.join'.

--- build-dev.py
+++ build-dev.py
@@ -14,6 +14,10 @@ CURRENT_DIRECTORY = os.getcwd()
 directories = os.listdir(CURRENT_DIRECTORY)
 NON_ANGULAR_DIRS = ['static', 'templates', 'venv']
 
+
+"""
+    Our desktop as we use 'os.path.join' to 'dist'.
+"""
 for directory in directories:
     if "." not in directory and directory not in NON_ANGULAR_DIRS:
         ANGULAR_PROJECT_PATH = os.path.join(CURRENT_DIRECTORY, directory)
